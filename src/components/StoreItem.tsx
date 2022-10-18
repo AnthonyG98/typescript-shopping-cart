cc2cd068c233f7f0a4b24f5eb02d6e4782d7e48c
Anthony Gonzalez <gonz.anthony6@gmail.com>
2022-10-18 15:46:37 -0700

    shopping cart

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,3 +1,5 @@
+import { formatCurrency } from "../utilities/formatCurrency"
+
 type StoreItemProps = {
     id: number,
     name: string,
@@ -7,6 +9,16 @@ type StoreItemProps = {
 
 export function StoreItem({id, name, price, imgUrl}: StoreItemProps){
     return (
-        <img src={imgUrl}/>
+        <>
+        <div className="store-prop-container">
+            <div className="store-prop-img-container">
+                <img src={imgUrl} className="store-prop-img" />
+            </div>
+            <div className="store-prop-info">
+                <p className="prop-title">{name}</p>
+                <p className="prop-price">{formatCurrency(price)}</p>
+            </div>
+        </div>
+        </>
     )
 }
