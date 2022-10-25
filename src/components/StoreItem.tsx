aa2751ed156e646032995c120cadcd40267c03b9
Anthony Gonzalez <gonz.anthony6@gmail.com>
2022-10-25 16:32:15 -0700

    UseContext

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -8,7 +8,7 @@ type StoreItemProps = {
 }
 
 export function StoreItem({id, name, price, imgUrl}: StoreItemProps){
-    const quantity = 0;
+    const quantity = 3;
     return (
         <>
         <div className="store-prop-container">
@@ -21,7 +21,17 @@ export function StoreItem({id, name, price, imgUrl}: StoreItemProps){
             </div>
             <div className="add-btn-container">
                 {
-                    quantity === 0 ? <button className="add-btn">ADD TO CART</button> : null
+                    quantity === 0 ? <button className="add-btn">ADD TO CART</button> : 
+                    <div className="adjust-btn-container">
+                        <div className="adjust-btn">
+                            <button className="adjust">-</button>
+                            <p>{quantity} in cart.</p>
+                            <button className="adjust">+</button>
+                        </div>
+                        <div className="remove-btn">
+                            REMOVE
+                        </div>
+                    </div>
                 }
             </div>
         </div>
